2b2bf00941b950e91e5ed2259d37d914b7fbad66
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-26 20:32:39 +0700

    Create april5.sh

---
+++ module/april5.sh
@@ -0,0 +1,8 @@
+
+
+toilet -f big -F gay MyProfil | lolcat
+echo [◇]=============================================[◇] | lolcat
+echo      [•] Author : MR-Xyaa                         
+echo      [•] FB     : Xyaa Xyaa                         
+echo      [•] GitHub : github.com/MR-Xyaa              
+echo [◇]=============================================[◇] | lolcat
