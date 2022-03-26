6ef92a9a2ffe0287a0eb4fa695a883a34853566a
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 00:28:57 +0700

    Update april10.py

--- module/april10.py
+++ module/april10.py
@@ -5,7 +5,7 @@ def mengetik(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(random.random() * 0.1)
+        time.sleep(random.random() * 0.01)
 
 mengetik('///////////////////////////////////////')
 mengetik('///////////////////////////////////////')
