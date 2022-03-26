17f53a48274b154ee1d4e9ab68c6b2b82dab8f57
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-26 19:27:05 +0700

    Create april.sh

---
+++ module/april.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#install bahan
+
+figlet Install Bahan | lolcat
+sleep 3
+
+pkg update && pkg upgrade
+pkg install sl
+pip install request mechanize
+python -m pip install -r requirements.txt
+
+figlet selesai | lolcat
+sleep 1
+
+echo WWW.XNXX.COM/ |lolcat
+sleep 5
+echo WWW.PORNHUB.COM |lolcat
+sleep 5
+
+sl
+cleaar
+echo LOADING... | lolcat
+sleep 5
+
+toilet -f big -F gay MyProfil | lolcat
+echo "<=============================================>" | lolcat
+echo "[•] Author : MR-Xyaa                         
+echo "[•] FB     : Xyaa Xyaa                         
+echo "[•] GitHub : github.com/MR-Xyaa              
+echo "<=============================================>" | lolcat
