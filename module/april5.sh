2fd0282ea180adfdfe3368329333feea76d681b7
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 00:29:54 +0700

    Update april5.sh

--- module/april5.sh
+++ module/april5.sh
@@ -2,12 +2,13 @@
 
 toilet -f big -F gay MyProfil | lolcat
 echo [◇]=============================================[◇] | lolcat
-echo      [•] Author : MR-Xyaa                         
-echo      [•] FB     : Xyaa Xyaa                         
-echo      [•] GitHub : github.com/MR-Xyaa              
+echo      [•] Author__: MR-Xyaa                         
+echo      [•] FB______: Xyaa Xyaa                         
+echo      [•] GitHub__: github.com/MR-Xyaa              
 echo [◇]=============================================[◇] | lolcat
 sleep 5
 echo ■■■■■■■■■■■■■■■■■■■■■| lolcat
 sleep 2
 echo Menginstall info...lolcat
+sleep 5
 
