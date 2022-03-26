515b715693a4560d76faeb1ceb5fbe60335bd67f
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-26 20:58:29 +0700

    Create april7.py

---
+++ module/april7.py
@@ -0,0 +1,22 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('●=======================================================●')
+    mengetik('NAMA___________: IBNU UMAR SYAID')
+    mengetik('GITHUB_________: HTTPS://GITHUB.COM/MR-Xyaa')
+    mengetik('FACEBOOK_______: XYAA XYAA')
+    mengetik('UMUR___________: 17')
+    mengetik('ALAMAT_________: PLUTO')
+    mengetik('PACAR__________: APRILLIA SURYATIMAH')
+    mengetik('HOBI___________: COLI')
+    mengetik('VIDIO FAVORIT__: BOKEP')
+    mengetik('PEKERJAAN______: TUKANG NGIMPI')
+mengetik('●=======================================================●')
+mengetik('☆')
+mengetik('☆')
+mengetik('DAH LAH MALES NGETIK:v')
