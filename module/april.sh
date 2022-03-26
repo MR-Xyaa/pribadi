aef4c2de40fc3bad9315718ca20b34f657ba54a9
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-26 20:33:28 +0700

    Update april.sh

--- module/april.sh
+++ module/april.sh
@@ -11,9 +11,3 @@ echo LOADING... | lolcat
 sleep 2
 sl
 clear
-toilet -f big -F gay MyProfil | lolcat
-echo [◇]=============================================[◇] | lolcat
-echo      [•] Author : MR-Xyaa                         
-echo      [•] FB     : Xyaa Xyaa                         
-echo      [•] GitHub : github.com/MR-Xyaa              
-echo [◇]=============================================[◇] | lolcat
