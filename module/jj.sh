cf0fb5edcfc60fc4042bd7e64f80f5522f8f2e41
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-26 23:33:54 +0700

    Create jj.sh

---
+++ module/jj.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo {●}======================================================={●} | lolcat
+echo       ■    NAMA      : IBNU UMAR SYAID                     ■ | lolcat
+echo       ■    GITHUB    : HTTPS://GITHUB.COM/MR-Xyaa          ■ | lolcat
+echo       ■    FACEBOOK  : XYAA XYAA                           ■ | lolcat
+echo       ■    UMUR      : 17                                  ■ | lolcat
+echo       ■    ALAMAT    : PLUTO                               ■ | lolcat
+echo       ■    PACAR     : APRILLIA SURYATIMAH                 ■ | lolcat
+echo       ■    HOBI      : COLI                                ■ | lolcat
+echo       ■    VIDIO FAV : BOKEP                               ■ | lolcat
+echo       ■    PEKERJAAN : TUKANG NGIMPI                       ■ | lolcat
+echo {●}======================================================={●} | lolcat
+echo ... | lolcat
+echo ... | lolcat
+echo DAH LAH MALES NGETIK ANJING:v | lolcat
